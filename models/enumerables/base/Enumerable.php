64809daa0fb37dbe0cc865c800b62a0ca7c98a0b
lexxorlov <aaorlov88@gmail.com>
2016-10-23 14:14:28 +0300

    Added base class

---
+++ models/enumerables/base/Enumerable.php
@@ -0,0 +1,40 @@
+<?php
+namespace common\models\enumerables\base;
+/**
+ * Enumerable is the base class for all enumerable types.
+ *
+ * To define an enumerable type, extend Enumberable and define string constants.
+ * Each constant represents an enumerable value.
+ * The constant name must be the same as the constant value.
+ * For example,
+ * <pre>
+ * class TextAlign extends Enumerable
+ * {
+ *     const Left='Left';
+ *     const Right='Right';
+ * }
+ * </pre>
+ * Then, one can use the enumerable values such as TextAlign::Left and
+ * TextAlign::Right.
+ *
+ * @author Alexey Orlov <aaorlo88@gmail.com>
+ */
+abstract class Enumerable
+{
+    /**
+     * Get label of the enumerable
+     *
+     * @static
+     * @param $value
+     * @return mixed
+     */
+    abstract public static function get($value);
+
+    /**
+     * Get list of the enumerable
+     *
+     * @static
+     * @return array
+     */
+    abstract public static function listData();
+}
